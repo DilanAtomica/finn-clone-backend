bf8bb51e12c945948f19f48ad0ee83c2bdc5a918
dilan <dilan-shwane@hotmail.com>
2023-07-26 17:09:14 +0200

    added firebase files and .idea to gitignore

--- routes/users.ts
+++ routes/users.ts
@@ -1,11 +1,14 @@
 import {Request, Response} from "express";
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 const express = require("express");
 const router = express.Router();
 const {db} = require("../utils/DBconnect");
+const {auth} = require("../firebase");
 
-router.get("/login", async(req: Request, res: Response) => {
+router.post("/login", async(req: Request, res: Response) => {
     try {
-
+        await createUserWithEmailAndPassword(auth, "dilan@hotmail.com", "123asd123asD");
+        res.status(200).json({message: "Logged in Successfully!"});
     } catch (error) {
         res.status(400).json({error: error});
     }
